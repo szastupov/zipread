413ff61d64852489a5a1f97ebd9197ba70abc403
Stepan Zastupov <stepan.zastupov@gmail.com>
2015-05-17 18:42:59 +0300

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,34 @@
+var Archive = require("../lib/archive.js");
+var fs = require("fs");
+var assert = require("assert");
+
+var testfiles = fs.readdirSync(__dirname + "/testfiles").map(function (f) {
+    return "testfiles/" + f;
+});
+
+describe ("Archive", function () {
+    var zip;
+
+    beforeEach(function () {
+        zip = new Archive(__dirname + "/test.zip");
+    });
+
+    afterEach(function () {
+        zip.close();
+    });
+
+    it ("should load archive", function () {
+        testfiles.forEach(function(f) {
+            assert(zip.files[f], f + " not found in archive");
+        });
+    });
+
+    it ("should decompress files", function () {
+        testfiles.forEach(function(f) {
+            var orig = fs.readFileSync(__dirname + "/" + f);
+            var res = zip.readFileSync(f);
+            assert.deepEqual(orig, res);
+        });
+    });
+
+});
